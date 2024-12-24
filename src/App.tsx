c88568a897bcab5184a2502b50836dae8198437a
mtkumar123 <mtkumar@ncsu.edu>
2024-12-24 14:19:23 +0200

    update deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () => {
     <div className="flex justify-center h-screen">
       <div className="pt-[200px]">
         <TestimonialCard
-          image="../img/profile-thumbnail.png"
+          image="img/profile-thumbnail.png"
           name="Sarah Dole"
           username="@sarahdole"
           message="I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!"
